fa97a166996d5703b8ed8b979b543df4a5768bf9
Kostas Kapetanakis <kapekost@gmail.com>
2018-01-21 16:59:24 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-// const NexmoConversation = require('./node_modules/nexmo-conversation/lib/sdk');
-// const nexmoConversation = new NexmoConversation();
-
 const express = require('express')
 const app = express();
 const bodyParser = require("body-parser");

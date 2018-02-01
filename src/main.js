4121c86efdc6520d5f01ead7cb75c2aac1dd7893
Kostas Kapetanakis <kapekost@gmail.com>
2018-02-01 00:17:16 +0000

    remove comments

--- src/main.js
+++ src/main.js
@@ -7,10 +7,6 @@ app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
     const duck = new Duck(req, res);
-    // res.send(JSON.stringify({
-    //     "speech": res,
-    //     "displayText": res
-    // }))
 });
 
 app.listen(8000, () => console.log('Duck service!'))

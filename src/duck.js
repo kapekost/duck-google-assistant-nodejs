991e7bc249d1b3c6ba2af46a6a728892418df28d
Kostas Kapetanakis <kapekost@gmail.com>
2018-06-19 20:28:02 +0100

    Integration/nexmo stitch v2 (#2)
    * update flow API
    
    * placeholders
    
    * cleanup

--- src/duck.js
+++ src/duck.js
@@ -1,5 +1,6 @@
-const DialogflowApp = require('actions-on-google').DialogflowApp;
+
 const Nexmo = require('nexmo');
+
 const ConversationClient = require('nexmo-stitch');
 
 class Duck {
@@ -23,10 +24,7 @@ class Duck {
             acl: { "paths": { "/v1/users/**": {}, "/v1/conversations/**": {}, "/v1/sessions/**": {}, "/v1/devices/**": {}, "/v1/image/**": {}, "/v3/media/**": {}, "/v1/applications/**": {}, "/v1/push/**": {} } }
         });
         console.log(jwt);
-        const app = new DialogflowApp({
-            request: request,
-            response: response
-        });
+
         const MORNING_INTENT = 'input.morning';
         const LOGIN_INTENT = 'input.login'
 
@@ -40,27 +38,23 @@ class Duck {
                     diplay_url: "https://randomuser.me/api/portraits/men/81.jpg"
                 },
                 () => {
-                    app.tell(`good morning to you too!, new user is ready`);
+                    app.ask(`good morning to you too!, new user is ready`);
                 });
         }
 
-        function loginIntent(app) {
-            conversationClient.login(jwt).then((stitchApp) => {
-                console.log('logged in' + jwt);
-                return stitchApp.newConversationAndJoin({ display_name: "miss Duck" })
+        app.intent('login', conv => {
+            // conv.ask(`Sure, let's login!`);
+            return conversationClient.login(jwt).then((stitchApp) => {
+                console.log('logged in');
+                conv.ask(`logged in as Kostas!`);
+                return stitchApp.newConversationAndJoin({ display_name: "Stitch Duck" })
                     .then((conversation) => {
-                        current_conversation = conversation;
-                        app.tell(`welcome to the stitch conversation!`);
+                        console.log('created conversation' + conversation.display_name);
+                        conv.ask(`Welcome to the conversation ${conversation.display_name}`);
+                        this.conversation = conversation;
                     });
             }).catch(error => console.log(error));
-        }
-
-
-        const actionMap = new Map();
-        actionMap.set(MORNING_INTENT, morningIntent);
-        actionMap.set(LOGIN_INTENT, loginIntent);
-        app.handleRequest(actionMap);
-
+        })
     }
 }
 

52c468ab221ec411affcd143136e91e3f1d478e9
Kostas Kapetanakis <kapekost@gmail.com>
2018-12-08 13:44:16 +0000

    update comments

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ const Duck = require('./duck');
 const { dialogflow } = require('actions-on-google');
 const app = dialogflow();
 
+//start the server and bind our core logic with the dialogflow
 new Duck(app);
-
 express().use(bodyParser.json(), app).listen(8000, () => console.log('Duck service! port:8000'))
